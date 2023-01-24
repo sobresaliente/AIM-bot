bd637a86afa7426a3a073d38a622045bdddcb2ed
Daniel Toint <Gedztodu@gmail.com>
2023-01-24 03:23:38 +0200

    Implement tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,9 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
       <TodoList />
     </div>
   );
